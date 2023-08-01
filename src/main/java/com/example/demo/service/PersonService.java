718a9374187520ab951def1fd5598f510db88be4
bassemibrahemmohamed <bassemibrahemmohamed@yahoo.com>
2023-08-01 11:00:00 +0300

    tryyyyedmessage

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -29,7 +29,7 @@ public class PersonService {
 	 */
 	@Autowired
 	public PersonService(PersonDao personDao) {
-		System.out.println("adcfdacadscad");
+		System.out.println("scad");
 		this.personDao = personDao;
 	}
 	
